72c3fc403df7e15c6d0bea09c555aa435c4f881d
250399 <kunaldeshmukh2503@gmail.com>
2020-07-24 06:33:16 -0700

    [Kunal] ADD: created views for listing products

--- products/views.py
+++ products/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.respose import Response
+from rest_framework.generics import ListAPIView
+from django.models import connection as conn
+from serializers import ProductListSerializer as prod_serializer
+
+# Create your views here.
+class BookListView(ListAPIView):
+
+    def get_queryset(self):
+        cursor = conn.cursor()
+        book_list = cursor.execute("select * from product")
+        serializer = prod_serializer(book_list)
+        return serializer.data
