a8045604168d2fc5b9ba08283a4f6f035689d285
250399 <kunaldeshmukh2503@gmail.com>
2020-07-24 13:22:43 -0700

    [Kunal] REFACTORED: view to use APIView instead of viewset

--- products/views.py
+++ products/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.respose import Response
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from django.models import connection as conn
 from serializers import ProductListSerializer as prod_serializer
+from .models import Product
 
 # Create your views here.
-class BookListView(ListAPIView):
+class BookListView(APIView):
 
-    def get_queryset(self):
-        cursor = conn.cursor()
-        book_list = cursor.execute("select * from product")
-        serializer = prod_serializer(book_list)
-        return serializer.data
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = prod_serializer(book_list, many=True)
+        return Response(serializer.data)
