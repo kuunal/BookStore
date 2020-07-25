6a36017320ca3ec31f9a57112ef06c414b747766
250399 <kunaldeshmukh2503@gmail.com>
2020-07-25 10:21:47 -0700

    [Kunal] ADD: Simple login API without otp

--- bookstore/login/views.py
+++ bookstore/login/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.db import connection as conn
+from .serializer import LoginSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
 class LoginView(APIView):   
-
+    serializer_class = LoginSerializer
 
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
-        cursor = conn.cursor()
-        cursor.execute("""select count(*) from users where email = %s and password = %s """ % (email,password))
-        count = cursor.fetchone()
-        return Response(200) if count > 0 else Response(401)
+        serializer = LoginSerializer(data = request.data)
+        if serializer.is_valid():
+            cursor = conn.cursor()
+            cursor.execute(f'select count(*) from users where email = %s and password = %s ' , (email,password))
+            count = cursor.fetchone()
+            print(count)
+            if count[0] > 0:
+                return Response(200)  
+        return Response(401)
