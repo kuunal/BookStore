77affb1c6480e08e2c61836cf972405374ee241b
250399 <kunaldeshmukh2503@gmail.com>
2020-07-25 19:56:04 -0700

    ADD: twilio credentials to .env

--- bookstore/login/otp.py
+++ bookstore/login/otp.py
@@ -1,5 +1,6 @@
 import random
 from twilio.rest import Client
+from bookstore import settings
 
 def send_otp(phone_no):
 
@@ -10,13 +11,13 @@ def send_otp(phone_no):
     for characters in range(number_of_character_in_otp):
         random_otp+=random.choice(characters_for_otp)
 
-    account_sid = 'AC24101a599eeb4552cb56f52fd8c06619'
-    auth_token = '27265addf7e0077bbb7b58c86b000faf'
+    account_sid = settings.TWILIO_ACCOUNT_SID
+    auth_token = settings.TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
                         body = f'Your otp is {random_otp}',
-                        from_='+17372147866',
+                        from_= settings.TWILIO_NUMBER,
                         to='phone_no'
                     )
 
