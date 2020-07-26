3fbef816512e8b3c103e57f4915a520095fe7d1d
250399 <kunaldeshmukh2503@gmail.com>
2020-07-26 12:28:53 -0700

    [Kunal] ADD: Storing otp to database

--- bookstore/login/otp.py
+++ bookstore/login/otp.py
@@ -1,7 +1,11 @@
 import random
 from twilio.rest import Client
 from bookstore import settings
+from django.db import connection as conn
+from datetime import datetime
+from django.utils import timezone
 
+ 
 def send_otp(phone_no):
 
     characters_for_otp = '1234567890abcdefghijklmnopqrstuvwxyz'
@@ -20,6 +24,10 @@ def send_otp(phone_no):
                         from_= settings.TWILIO_NUMBER,
                         to='phone_no'
                     )
-
+    try:
+        query = 'insert into otp_history values (%s, %s, %s)' % (phone_no, random_otp, timezone.now())
+        cursor = conn.cursor()
+    finally:
+        cursor.close()
 
     return random_otp
