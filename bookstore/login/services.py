cac8e0e0507812507130cc3babf394c0d6036487
250399 <kunaldeshmukh2503@gmail.com>
2020-07-27 00:47:21 -0700

    [Kunal] ADD: Functionality to block user for many tries

--- bookstore/login/services.py
+++ bookstore/login/services.py
@@ -18,3 +18,27 @@ def check_if_otp_generated_for_more_than_limit_for_user(phone_no):
             raise ValidationError("You have tried too many times. Please come back again tommorow")
     finally:
         cursor.close()
+
+
+def calculate_remaining_block_time(block_time):
+    remaining_block_time = (timezone.now() - block_time).total_seconds()//3600
+    print(remaining_block_time, block_time)
+    return remaining_block_time 
+                
+        
+
+def check_if_user_is_blocked(phone_no):
+    try:
+        current_time = timezone.now()
+        cursor = cn.cursor()
+        cursor.execute('select datetime from otp_history where phone_no = %s and otp = %s', [phone_no, "blockd"] )
+        block_time = cursor.fetchall()
+        if block_time :
+            block_time = block_time[:1][0][0]
+
+            remaining_time = calculate_remaining_block_time(block_time)
+            if remaining_time < 0:
+                raise ValidationError("You are blocked for trying so many times. Please come back after "+ str(abs(remaining_time)) +" Hours")
+
+    finally:
+        cursor.close()
--- bookstore/login/views.py
+++ bookstore/login/views.py
@@ -6,15 +6,17 @@ from rest_framework.response import Response
 from .otp import send_otp, gen_otp
 from .tasks import send_otp_to_user_while_login
 from datetime import datetime
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 from datetime import timedelta
-from .services import check_if_otp_generated_for_more_than_limit_for_user
-
+from .services import check_if_otp_generated_for_more_than_limit_for_user, check_if_user_is_blocked
+from bookstore.redis_setup import get_redis_instance
 
 # Create your views here.
 
 class LoginView(APIView):   
     serializer_class = LoginSerializer
+    redis_instance  = get_redis_instance()
 
     def post(self, request):
         login_id = request.data['login_id']
@@ -27,16 +29,18 @@ class LoginView(APIView):
                 phone_no = cursor.fetchone()
                 phone_no = phone_no[0]
                 if phone_no:
-                    check_if_otp_generated_for_more_than_limit_for_user(phone_no)
+                    try:
+                        check_if_otp_generated_for_more_than_limit_for_user(phone_no)
+                        check_if_user_is_blocked(phone_no)
+                    except ValidationError as e:
+                        return Response({'status':400,'message':str(e)})
                     random_otp = gen_otp()
-                    # send_otp_to_user_while_login.delay(phone_no, random_otp) 
+                    send_otp_to_user_while_login.delay(phone_no, random_otp) 
                     cursor.execute('insert into otp_history(phone_no, otp, datetime) values(%s, %s, %s)',(phone_no, random_otp, timezone.now()))
-                    
-            
-                    return Response(404)
-                    # return Response(200)
+                    LoginView.redis_instance.set(phone_no, random_otp)
+                    return Response(200)
             except Exception as e:
-                print(e)
+                return Response(str(e))
             finally:
                 cursor.close()
         return Response(401)
