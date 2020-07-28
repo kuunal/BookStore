0f73ae575697c1ab62b216e8ef88c06a8b02a635
250399 <kunaldeshmukh2503@gmail.com>
2020-07-28 00:32:43 -0700

    [Kunal] REFACTOR: Returns dummy data for dev purpose

--- bookstore/wishlist/services.py
+++ bookstore/wishlist/services.py
@@ -1,7 +1,8 @@
-from settings.redis_setup import get_redis_instance
+from bookstore.redis_setup import get_redis_instance
 
 
 def get_current_user():
-    redis_instance = get_redis_instance
-    for key in redis_instance.scan_iter():
-        return redis_instance.get(key)
+    # redis_instance = get_redis_instance()
+    # for key in redis_instance.scan_iter():
+    #     return redis_instance.get(key)
+    return 2
