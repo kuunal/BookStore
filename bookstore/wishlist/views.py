fe3f9b532a900cd8f14b0dc6633cf1c57811d814
250399 <kunaldeshmukh2503@gmail.com>
2020-07-27 22:26:20 -0700

    [Kunal] ADD: View for Wishlist

--- bookstore/wishlist/views.py
+++ bookstore/wishlist/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import WishListSerializer
+from .models import WishListModel
+from rest_framework.response import Response
 
 # Create your views here.
+class WishListView(APIView):
+    serializer_class = WishListSerializer
+    def get(self, request):
+        wishlist = WishListModel.objects.all() 
+        serializer = WishListSerializer(data = wishlist)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(400)
