af27de338d32db98d99a6b6e31398e8c88220bca
250399 <kunaldeshmukh2503@gmail.com>
2020-07-27 22:22:24 -0700

    [Kunal] ADD: Serialzier for Wishlist

--- bookstore/wishlist/serializer.py
+++ bookstore/wishlist/serializer.py
@@ -1,11 +1,14 @@
 from rest_framework.serializers import Serializer
-
+from products.serializers import ProductSerializer
 
 class UserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length = 100)  
     email = serializers.EmailField(max_lenth = 100)
     phone_no = serializers.CharField(max_length=13)
 
-
+class WishListSerializer(serializers.Serializer):
+    user_id  = UserSerializer(read_only=True)
+    product_id = ProductSerializer(read_only=True)
+    
 
 
