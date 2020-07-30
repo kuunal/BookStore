b9b42889fcfe06398ea1d23563f2a2651ab4b127
250399 <kunaldeshmukh2503@gmail.com>
2020-07-30 14:50:23 -0700

    [Kunal] ADD: Get method for cart

--- bookstore/cart/models.py
+++ bookstore/cart/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from wishlist.models import WishListsManager
 
-# Create your models here.
+
+class CartManager:
+    
+    @staticmethod
+    def all(params=None):
+        query = 'select * from cart where user_id = %s'
+        return WishListsManager.all(query,(params,), CartModel)
+        
+
+    @staticmethod
+    def get(id, user_id):
+        pass
+
+    @staticmethod
+    def insert(obj):
+        pass
+
+    @staticmethod
+    def delete(id):
+        pass
+
+    @staticmethod
+    def update(id):
+        pass
+
+class CartModel:
+    objects = CartManager()
+
+    def __init__(self, id=None, user_id=None, product_id=None, total=None):
+        self.id = id
+        self.user_id = user_id
+        self.product_id = product_id
+        self.total = total
--- bookstore/cart/views.py
+++ bookstore/cart/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from products.serializer import ProductSerializer
+from products.models import Product
+from rest_framework.response import Response
+from django.db import connection, IntegrityError
+from login.services import get_current_user
+from .models import CartModel
 
-# Create your views here.
+class CartView(APIView):
+    def get(self, request, id=None):
+        user_id  = get_current_user(request)
+        if id:
+            try:
+                cart_items = CartModel.objects.get(id, user_id)
+                products = Product.objects.get(str(wishlist.product_id))
+            except IndexError:
+                return Response(get_response_code('invalid_product_id'))
+        else:
+            cart_items = CartModel.objects.all(params=user_id)
+            products = Product.objects.filter(cart_items)
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
+
