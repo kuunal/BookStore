a0bba710922ba11870318e27e2d7e2ac7e35af90
250399 <kunaldeshmukh2503@gmail.com>
2020-07-30 01:43:30 -0700

    [Kunal] ADD: Created new endpoint for adding product in wishlist

--- bookstore/wishlist/urls.py
+++ bookstore/wishlist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers 
-from .views import WishListView
+from .views import WishListView, add_to_wishlist
 
 
 urlpatterns = [
     path('/', WishListView.as_view()),
+    path('/add', add_to_wishlist),
     path('/<str:id>', WishListView.as_view()),
 ]
 
