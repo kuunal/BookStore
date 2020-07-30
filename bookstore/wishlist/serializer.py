f055a804a4b09adb26051f8cdb5aa2e86aad2998
250399 <kunaldeshmukh2503@gmail.com>
2020-07-30 01:42:50 -0700

    [Kunal] REFACTOR: Removed serializer for Wishlist

--- bookstore/wishlist/serializer.py
+++ bookstore/wishlist/serializer.py
@@ -3,20 +3,6 @@ from products.serializer import ProductSerializer
 from .models import WishListModel
 from login.services import get_current_user
 
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)  
-    email = serializers.EmailField(max_length=100)
-    phone_no = serializers.CharField(max_length=13)
-
-class WishListSerializer(serializers.Serializer):
-    product_id = serializers.IntegerField()
-
-    def create(self, validated_data):
-        wishlist_obj = WishListModel(**validated_data)
-        wishlist_obj.save()
-        return wishlist_obj 
-
-
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     author = serializers.CharField(max_length=255)
