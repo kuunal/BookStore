5d867dd7309e7089d03b2ef216d037ea41130f98
250399 <kunaldeshmukh2503@gmail.com>
2020-08-01 00:22:45 -0700

    [Kunal] ADD: Created model for order

--- bookstore/orders/models.py
+++ bookstore/orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class OrderManager:
+    pass
+
+
+
+class OrderModel:
+    objects = OrderManager()
+
+    def __init__(self, id=None, order_id=None, user_id=None, quantity=None, total=None, address=None):
+        self.id = id
+        self.order_id = order_id
+        self.user_id = user_id 
+        self.quantity = quantity
+        self.total = total
+        self.address = address
+
+
+
