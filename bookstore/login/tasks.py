aee47aebd226d52d541686426754e1ee98b23d88
250399 <kunaldeshmukh2503@gmail.com>
2020-08-03 11:17:31 -0700

    [Kunal] ADD: Functionality to Send mail to user asynchronously

--- bookstore/login/tasks.py
+++ bookstore/login/tasks.py
@@ -1,11 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .otp import send_otp
+from .models import LoginManager
+from .send_mail import send_custom_mail
 
 
-MESSAGE_FOR_LOGIN="Your otp is "
-
+SUBJECT_FOR_ORDER_PLACED = "Your order has been placed"
+TEMPLATE_NAME = 'login/email_for_order_placed.html'
 
 @shared_task
 def send_otp_to_user_while_login(phone_no, random_otp):
     send_otp(phone_no, random_otp)
+
+@shared_task
+def order_placed_mail_to_user(obj, total, user_id):
+    user_email = LoginManager.get(user_id)
+    print(user_email)
+    send_custom_mail(obj, TEMPLATE_NAME, SUBJECT_FOR_ORDER_PLACED, user_email, total)
+
