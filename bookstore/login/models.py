fd35e6f0b7772ed811cdd3e89f54d73dd4cc57d4
250399 <kunaldeshmukh2503@gmail.com>
2020-08-02 20:17:24 -0700

    [Kunal] ADD: Functionality to get user email by id

--- bookstore/login/models.py
+++ bookstore/login/models.py
@@ -1,5 +1,13 @@
-from django.db import models
+from django.db import models, connection
 
 # Create your models here.
 
+class LoginManager:
 
+    @staticmethod
+    def get(user_id):
+        try:
+            cursor = connection.cursor()
+            cursor.execute('Select email from users where id = %s', (user_id,))
+            user_email = cursor.fetchall()[0]
+            return user_email
