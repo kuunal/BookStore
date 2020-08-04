16a66d59a477538fa9ed9604662d6b71509f186a
250399 <kunaldeshmukh2503@gmail.com>
2020-08-03 23:43:18 -0700

    [Kunal] ADD: Stataus codes for Cart

--- bookstore/response_codes.py
+++ bookstore/response_codes.py
@@ -1,16 +1,19 @@
 TOTAL_NUMBER_OF_OTP_CHARACTER = 6
-JWT_EXPIRATION_TIME=300
+JWT_EXPIRATION_TIME=30000
 OTP_EXPIRY_TIME=300
 OTP_BLOCK_TIME=3600 
 
 responses= {
      'invalid_login' :{'status':400, 'message':'Invalid login_id'},
      'login_required' : {'status':401 ,'message' : 'Please login first'},
+     'jwt_auth_error' : {'status' : 401, 'message' : 'Invalid Token'},
+     'jwt_decode_error' : { 'status' : 400, 'message' : 'JWT decode error'},
+     'jwt_signature_expired' : {'status' : 401, 'message':'OTP time expired! Please request for OTP again.'}, 
      'verify_response' : {'status':200, 'message':'Successfully verified'},
      'invalid_product_id' : {'status':400, 'message':'No such product'},
      'added_to_wishlist' : {'status':200, 'message':'Added to wishlist'},
      'invalid_product' : {'status':400, 'message':'invalid product'},
-     'deleted_wishlist_item' : {'status':200, 'message':'Deleted from wishlist'},
+     'deleted_wishlist_item' : {'status':200, 'message':'Removed from wishlist'},
      'otp_invalid' : {'status':401,'message':'OTP is invalid!'},
      'otp_not_generated' : {'status':401,'message':'You havent requested for OTP!'},
      'otp_sent' : {'status':200, 'message':'Please verify by entering OTP sent to you'},
@@ -20,7 +23,11 @@ responses= {
      'added_to_wishlist' : {'status':200, 'message':'Added to cart'},
      'updated_quantity': {'status':200, 'message':'Updated quantity successfully'},
      'out_of_stock' : {'status':400, 'message':'Product out of stock for that quantity'},
-     'order_placed' : {'status' : 200, 'message' : 'Order Placed'}
+     'order_placed' : {'status' : 200, 'message' : 'Order Placed'},
+     'not_available' : {'status' : 400, 'message' : 'Product not available'},
+     'item_not_in_cart':{'status':400, 'message':'No such item in cart'},
+     'removed_cart_item' : {'status':200, 'message':'Removed from cart'},
+
     }
 
 
