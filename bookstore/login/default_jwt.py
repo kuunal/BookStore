7e6bafcf22aaac5264b251be8ef0ab9b2a4cef8f
250399 <kunaldeshmukh2503@gmail.com>
2020-08-03 23:32:45 -0700

    [Kunal] REFACTOR: Proper exception ahndling for jwt

--- bookstore/login/default_jwt.py
+++ bookstore/login/default_jwt.py
@@ -3,22 +3,23 @@ from rest_framework import authentication, exceptions
 from bookstore import settings
 from datetime import timedelta
 from django.utils import timezone
-
-
+from bookstore.book_store_exception import BookStoreError
+from response_codes import get_response_code
 
 def jwt_decode(token):
+    print(settings.JWT_SECRET_KEY)
     if not token:
-        raise exceptions.AuthenticationFailed("Your token is invalid")  
+        raise BookStoreError(get_response_code('login_required'))  
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY)
         user_email=payload.get('user_id')
         return user_email
     except jwt.DecodeError as identifier:   
-        raise exceptions.AuthenticationFailed("Your token is invalid")
+        raise BookStoreError(get_response_code('jwt_decode_error'))
     except jwt.ExpiredSignatureError as indentifier:
-        raise exceptions.AuthenticationFailed("your token is expired!")   
+        raise BookStoreError(get_response_code('jwt_expiration_error'))   
 
 def jwt_encode(user_id):
     return jwt.encode({'user_id':user_id,
-                    'exp': timezone.now() + timedelta(seconds=int(settings.JWT_EXPIRATION_TIME)),
+                    'exp': timezone.now() + timedelta(seconds=settings.JWT_EXPIRATION_TIME),
                     },settings.JWT_SECRET_KEY)
