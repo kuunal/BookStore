89f9ec1fd15a49e92453c586aa4bf29d59890a4b
250399 <kunaldeshmukh2503@gmail.com>
2020-08-08 01:30:40 +0530

    [Kunal] REFACTOR: Changed add_to_cart method to POST

--- bookstore/cart/views.py
+++ bookstore/cart/views.py
@@ -9,7 +9,7 @@ from login.services import get_current_user
 from .models import CartModel
 from response_codes import get_response_code
 from django.core.exceptions import ValidationError
-from .serializers import CartSerializer, CartOrderSerializer
+from .serializers import CartSerializer, CartOrderSerializer, CartAddSerializer
 from orders.services import get_latest_order_id
 from orders.models import OrderManager
 from login.services import login_required
@@ -40,46 +40,38 @@ class CartView(APIView):
             return Response(get_response_code('item_not_in_cart'))
         return Response(get_response_code('removed_cart_item'))
 
-    @swagger_auto_schema(request_body=CartOrderSerializer)
-    @login_required
-    def post(self, request, id=None):
-        user_id = get_current_user(request)
-        address = request.data['address']
-        if id:
-            result = CartModel.objects.get(id,user_id)
-        else:
-            result = CartModel.objects.all(user_id)
-        total = sum([total.price if total.quantity == 1 else total.price*total.quantity for total in result])
-        if len(result)==0:
-            raise BookStoreError(get_response_code('item_not_in_cart'))
-        id = get_latest_order_id()
-        OrderManager.insert(result, total, address, id)
-        return Response(get_response_code('order_placed'))
 
-
-product_id = openapi.Parameter('id',openapi.IN_QUERY, type=openapi.TYPE_INTEGER, required=True) 
-quantity = openapi.Parameter('quantity', openapi.IN_QUERY, required=True,type=openapi.TYPE_INTEGER) 
-
-@swagger_auto_schema(method='get' ,manual_parameters=[product_id, quantity],)
-@api_view(('GET',))
+@swagger_auto_schema(method='post', request_body=CartAddSerializer)
+@api_view(('POST',))
 @login_required
 def add_to_cart(request):
-    product_id = request.GET.get('id')
-    quantity = '1' if request.GET.get('quantity') == None else request.GET.get('quantity')
-    if not product_id.isnumeric() or not quantity.isnumeric() or product_id ==0 and quantity ==0:
-        raise BookStoreError(get_response_code('invalid_product'))
-    user_id  = get_current_user(request)
-    cart_item = CartModel(user_id=user_id, product_id=product_id, quantity=quantity)
-    cart_item.save()
-    return Response(get_response_code('added_to_wishlist'))
+    serializer = CartAddSerializer(data= request.data)
+    if serializer.is_valid():
+        user_id  = get_current_user(request)
+        serializer.save(user_id=user_id)
+        return Response(get_response_code('added_to_wishlist'))
+    return Response(serializer.data)
+
 
 @api_view(('GET',))
 @login_required
 def get_view(request):
     user_id  = get_current_user(request)
-    result = CartModel.objects.all(user_id)
-    total = sum([total.price if total.quantity == 1 else total.price*total.quantity for total in result])
-    serializer = CartSerializer(result, many = True)
+    items = CartModel.objects.all(user_id)
+    total = sum([item.price if item.quantity == 1 else item.price*item.quantity for item in items])
+    serializer = CartSerializer(items, many = True)
     return Response({'cart':serializer.data, 'total':total})
 
-
+@swagger_auto_schema(method='post', request_body=CartOrderSerializer)
+@api_view(('POST',))
+@login_required
+def order(request):
+    user_id = get_current_user(request)
+    address = request.data['address']
+    items = CartModel.objects.all(user_id)
+    total = sum([item.price if item.quantity == 1 else item.price*item.quantity for item in result])
+    if len(result)==0:
+        raise BookStoreError(get_response_code('item_not_in_cart'))
+    id = get_latest_order_id()
+    OrderManager.insert(items, total, address, id)
+    return Response(get_response_code('order_placed'))
