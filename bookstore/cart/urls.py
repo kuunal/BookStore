c669df17a3acad98277d6fa4734e21bad4a79bc0
250399 <kunaldeshmukh2503@gmail.com>
2020-08-08 01:29:50 +0530

    [Kunal] ADD: Created seperate endpoint for ordering through cart

--- bookstore/cart/urls.py
+++ bookstore/cart/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CartView, add_to_cart, get_view
+from .views import CartView, add_to_cart, get_view, order
 
 urlpatterns = [
     path('/', get_view),
-    path('/add', add_to_cart),
+    path('/upsert', add_to_cart),
     path('/<int:id>', CartView.as_view()),
-
+    path('/order', order)
 ]
