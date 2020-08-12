513cb76c47062f5bf4ab108d1b51e2daddc98248
250399 <kunaldeshmukh2503@gmail.com>
2020-08-12 11:47:49 +0530

    [Kunal] ADD: Update redis when ordered items

--- bookstore/login/tasks.py
+++ bookstore/login/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 from .otp import send_otp
 from .models import LoginManager
 from .send_mail import send_custom_mail
+from product.models import Product
+from products.services import set_cache
 
 
 SUBJECT_FOR_ORDER_PLACED = "Your order has been placed"
@@ -14,6 +16,8 @@ def send_otp_to_user_while_login(phone_no, random_otp):
 
 @shared_task
 def order_placed_mail_to_user(obj, total, user_id, order_id, address):
+    products = Product.objects.all()
+    set_cache(products)
     user_email = LoginManager.get(user_id)
     send_custom_mail(obj, TEMPLATE_NAME, SUBJECT_FOR_ORDER_PLACED, user_email, total, order_id, address)
 
