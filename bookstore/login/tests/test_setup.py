d32956ac905947ecf80e4a0a956a799d0042cc80
250399 <kunaldeshmukh2503@gmail.com>
2020-08-17 01:46:58 +0530

    [Kunal] ADD: Unit Test cases for login

--- bookstore/login/tests/test_setup.py
+++ bookstore/login/tests/test_setup.py
@@ -6,13 +6,19 @@ class TestSetUp(APITestCase):
     def setUp(self):
         self.login_url = reverse('login')
         self.user_data={
-            'login_id':9004546038,  
+            'login_id':'kunaldeshmukh2503@gmail.com',  
             'password':'Kunal@123'
         }
         self.wrong_data={
-            'login_id':'wrong',  
+            'login_id':9232132222,  
             'password':'wrong@123'
         }
+        res = self.client.post(self.login_url, self.user_data, format='json')
+        import pdb 
+        pdb.set_trace()
+        otp = res.data['otp']
+        res = self.client.post(reverse('otp'),{'otp':otp})
+        self.jwt = res.data['jwt']
         return super().setUp()
 
     def tearDown(self):
--- bookstore/login/tests/test_view.py
+++ bookstore/login/tests/test_view.py
@@ -7,7 +7,14 @@ class TestViews(TestSetUp):
         res = self.client.post(self.login_url, self.user_data, format="json")
         self.assertEqual(res.status_code, 200)
 
-    # def test_given_valid_id_and_pass_when_correct_returns_respose_200(self):
-    #     res = self.client.post(self.login_url, self.wrong_data, format="json")
-    #     self.assertEqual(res.status_code, 401)
+    def test_given_valid_id_and_pass_when_incorrect_returns_respose_401(self):
+        res = self.client.post(self.login_url, self.wrong_data, format="json")
+        self.assertEqual(res.data['message'], 'invalid Id or pass')
 
+    def test_given_otp_when_valid_returns_json_token(self):
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        otp = res.data['otp']
+        res = self.client.post(reverse('otp'),{'otp':otp})
+        self.assertEqual(res.data['message'],'Successfully verified')
+        self.assertIsNotNone(res.data['jwt'])
+        
