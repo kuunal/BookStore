9f5ff36d0628e8cd877cc619d26b944605376548
250399 <kunaldeshmukh2503@gmail.com>
2020-09-12 16:42:20 +0530

    [Kunal;] ADD: Component for displaying Bookstore text and logo

---
+++ frontend/src/component/headers/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div >
+            <h1 style={headerStyle}><img src="https://public-v2links.adobecc.com/f01e3875-2a95-4975-7ad1-9a60aa079aec/component?params=component_id%3Abd36c005-5deb-46cb-b567-179223c388d2&params=version%3A0&token=1599288995_da39a3ee_de376559630c81d97346ca6266b6b24e858dbe3c&api_key=CometServer1"/>BookStore</h1>
+        </div>
+    )
+}
+
+const headerStyle = {
+    color:'white',
+    flex:'1',
+    alignItems:'center'
+}
