654a8e9cde206c90a2fe9ee846a183ea49d173d1
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-26 21:14:02 +0530

    [Kunal] REFACTOR: moved database queries to model

--- bookstore/login/views.py
+++ bookstore/login/views.py
@@ -11,7 +11,11 @@ from datetime import datetime
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from datetime import timedelta
-from .services import check_if_otp_generated_for_more_than_limit_for_user, check_if_user_is_blocked, login_required
+from .services import (
+    check_if_otp_generated_for_more_than_limit_for_user,
+    check_if_user_is_blocked,
+    login_required,
+)
 from bookstore.redis_setup import get_redis_instance
 from bookstore import settings
 from response_codes import get_response_code
@@ -22,72 +26,78 @@ from redis import DataError
 from bookstore.book_store_exception import BookStoreError
 from drf_yasg.utils import swagger_auto_schema
 from django.http import HttpResponse
+from .models import LoginManager
 
 # Create your views here.
 
-class LoginView(APIView):   
+
+class LoginView(APIView):
     serializer_class = LoginSerializer
- 
+
     @swagger_auto_schema(request_body=LoginSerializer)
     def post(self, request):
-        login_id = request.data['login_id']
-        password = request.data['password']
-        serializer = LoginSerializer(data = request.data)
+        login_id = request.data["login_id"]
+        password = request.data["password"]
+        serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             try:
                 cursor = conn.cursor()
-                phone_no = db.execute_sql("select phone_no from users where (email = %s or phone_no like %s) and password = %s ", (login_id, '%'+login_id, password), False)
+                phone_no = LoginManager.get_user(login_id, password)
                 if phone_no:
                     check_if_otp_generated_for_more_than_limit_for_user(phone_no)
                     check_if_user_is_blocked(phone_no)
                     random_otp = gen_otp()
-                    send_otp_to_user_while_login.delay(phone_no, random_otp)  
-                    db.execute_sql('insert into otp_history(phone_no, otp, datetime) values(%s, %s, %s)',(phone_no, random_otp, timezone.now()))
-                    response = get_response_code('otp_sent')
-                    response['Phone No.'] = phone_no
+                    send_otp_to_user_while_login.delay(phone_no, random_otp)
+                    LoginManager.store_onto_OTP_table(phone_no, random_otp)
+                    response = get_response_code("otp_sent")
+                    response["Phone No."] = phone_no
                     return Response(response)
                 else:
-                    return Response(get_response_code('login_failed'))
+                    return Response(get_response_code("login_failed"))
             except TypeError:
-                return Response(get_response_code('login_failed')) 
+                return Response(get_response_code("login_failed"))
+            except Exception:
+                return Response(get_response_code("generic_response"))
             finally:
                 cursor.close()
         return Response(serializer.errors)
 
 
 class VerifyOTPView(APIView):
-
     @swagger_auto_schema(request_body=OTPSerializer)
     def post(self, request):
-        phone_no = request.headers.get('x_phoneno')
-        otp = request.data['otp']
-        redis_instance  = get_redis_instance()
+        phone_no = request.headers.get("x_phoneno")
+        otp = request.data["otp"]
+        redis_instance = get_redis_instance()
         if not phone_no:
-            raise BookStoreError(get_response_code('no_headers'))
+            raise BookStoreError(get_response_code("no_headers"))
         try:
             cursor = conn.cursor()
-            original_otps = db.execute_sql('select otp, datetime from otp_history where phone_no = %s', [phone_no], True)
+            original_otps = LoginManager.get_otp_for_phone_no(phone_no)
 
             if original_otps:
-                latest_otp = original_otps[len(original_otps)-1][0]
-                latest_otp_send_time  = original_otps[len(original_otps)-1][1]  
-                elasped_time = (timezone.now()-latest_otp_send_time).total_seconds()
-                if otp == latest_otp and int(elasped_time) < int(settings.OTP_EXPIRY_TIME): 
-                    user_id = db.execute_sql('select id from users where phone_no = %s',[phone_no], True)[0][0]
+                latest_otp = original_otps[len(original_otps) - 1][0]
+                latest_otp_send_time = original_otps[len(original_otps) - 1][1]
+                elasped_time = (timezone.now() - latest_otp_send_time).total_seconds()
+                if otp == latest_otp and int(elasped_time) < int(
+                    settings.OTP_EXPIRY_TIME
+                ):
+                    user_id = LoginManager.get_user_id_from_phone_no(phone_no)
                     token = jwt_encode(user_id)
                     redis_instance.set(user_id, token)
-                    db.execute_sql('delete from otp_history where phone_no = %s', [phone_no])
-                    response = get_response_code('verify_response')
-                    response['token']=token
+                    LoginManager.delete_otp_for_authenticated_user(phone_no)
+                    response = get_response_code("verify_response")
+                    response["token"] = token
                     return Response(response)
                 else:
-                    return Response(get_response_code('otp_invalid'))
+                    return Response(get_response_code("otp_invalid"))
             else:
-                return Response(get_response_code('otp_not_generated'))
+                return Response(get_response_code("otp_not_generated"))
         finally:
             cursor.close()
 
-@api_view(('GET',))
+
+@api_view(("GET",))
 @login_required
 def logout(request):
     user_id = get_current_user(request)
@@ -95,5 +105,7 @@ def logout(request):
     try:
         redis_instance.delete(user_id)
     except DataError:
-        raise BookStoreError(get_response_code('login_required'))
-    return Response(get_response_code('logout'))
+        raise BookStoreError(get_response_code("login_required"))
+    except Exception:
+        return Response(get_response_code("generic_response"))
+    return Response(get_response_code("logout"))
