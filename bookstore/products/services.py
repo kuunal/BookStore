e231db0678fa613fbdbb253bfd88a39a68b25f71
kuunal <kunaldeshmukh2503@gmail.com>
2020-10-26 21:14:43 +0530

    [Kunal] REFACTOR: Made redis instance as global

--- bookstore/products/services.py
+++ bookstore/products/services.py
@@ -1,18 +1,17 @@
 import pickle
 from bookstore.redis_setup import get_redis_instance
 
+redis_instance = get_redis_instance()
+
+
 def set_cache(obj):
-    redis_instance = get_redis_instance()
     pickled_obj = pickle.dumps(obj)
-    redis_instance.set('products',pickled_obj)
+    redis_instance.set("products", pickled_obj)
 
 
 def get_cache_item():
-    redis_instance = get_redis_instance()
-    products = redis_instance.get('products')
+    products = redis_instance.get("products")
     if products:
         products = pickle.loads(products)
         return products
     return None
-
-
